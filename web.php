0bbcd87f77dfa51c7b3f3f74b0c48548a7515712
西山 <tatsuyasuukatuyou@gmail.com>
2020-06-06 10:13:06 +0000

    再提出

--- web.php
+++ web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-     Route::get('profile/create', 'ProfileController@add');
-     Route::get('profile/edit', 'ProfileController@edit');
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
